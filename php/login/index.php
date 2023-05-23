f748941950e76f40d9bab199edfba6c861632e8a
Giang Văn Lâm <van131361@gmain.com>
2023-05-23 17:28:31 +0700

    update index

--- php/login/index.php
+++ php/login/index.php
@@ -1,3 +1,40 @@
+<?php 
+session_start();
+$alert='';
+ if($_SERVER['REQUEST_METHOD']=='POST'){
+try
+{
+     $userName = $_POST['username'];
+     $userPass = $_POST['password'];
+    
+    
+    $_SESSION['username'] = $userName;
+    $_SESSION['password'] = $userPass;
+
+   
+    $conn = new PDO("mysql:host=localhost;dbname=btth02", 'root', '');
+    $sql = "select * from users where username = :username and password = :password";
+    
+    $stmt = $conn -> prepare($sql);
+    $stmt -> bindValue(':username',$userName,PDO::PARAM_STR);
+    $stmt -> bindValue(':password',$userPass,PDO::PARAM_STR);
+    $stmt ->execute();
+    $member = $stmt ->fetchAll();
+   
+    if($stmt -> rowCount()==1 and end($member[0])==1){
+        header('Location: ../studentAttendance/studentAttendance.php');
+    }
+    else if($stmt -> rowCount()==1 and end($member[0])==0)
+    {
+      header('Location: ../AttendanceTeacher/AttendanceTeacher.php'); 
+    }
+    else {
+       $alert = 'Vui lòng nhập đúng tài khoản hoặc mật khẩu!';
+    }
+} catch(PDOException $e){
+    echo 'Error: ' .$e->getMessage();
+}
+ }?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -49,4 +86,5 @@
     </form>
   </div>
 </body>
+
 </html>
